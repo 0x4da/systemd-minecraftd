3a029f746afc0690789ae41325c30c871726c0e5
David Cooke <me@dave.lc>
2021-04-27 16:47:23 +0100

    require root

--- src/main/java/lc/dave/systemdminecraftd/Main.java
+++ src/main/java/lc/dave/systemdminecraftd/Main.java
@@ -1,8 +1,19 @@
 package lc.dave.systemdminecraftd;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
 
 public final class Main extends JavaPlugin {
 
+    private final Logger logger = getSLF4JLogger();
+
+    @Override
+    public void onEnable() {
+        if(!"root".equals(System.getProperty("user.name"))) {
+            logger.error("systemd-minecraftd must run as root.");
+            Bukkit.shutdown();
+        }
+    }
 
 }
